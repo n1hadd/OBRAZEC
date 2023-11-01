f089178523b9d910c9f782208808a381374ca09f
n1hadd <148866867+n1hadd@users.noreply.github.com>
2023-11-01 17:38:07 +0100

    Add files via upload

--- nal7 - OBRAZEC/makePayment.js
+++ nal7 - OBRAZEC/makePayment.js
@@ -1,6 +1,25 @@
 function makePayment(event) {
     event.preventDefault(); // This prevents the form from submitting
 
+    // Check if delivery address inputs are filled
+    const inputs = document.querySelectorAll('#delivery-address input[type="text"]');
+    let isDeliveryAddressFilled = true;
+
+    inputs.forEach(input => {
+        if (input.value.trim() === '') {
+            isDeliveryAddressFilled = false;
+        }
+    });
+
+    if (!isDeliveryAddressFilled) {
+        Swal.fire({
+            icon: 'error',
+            title: 'Incomplete Delivery Address',
+            text: 'Please fill in all fields in the Delivery Address section.'
+        });
+        return; // Stop further execution
+    }
+
     const creditCardInput = document.getElementById('creditCard');
     const creditCardNumber = creditCardInput.value.replace(/\s/g, '');
 
@@ -27,6 +46,3 @@ function makePayment(event) {
         });
     }
 }
-
-
-
