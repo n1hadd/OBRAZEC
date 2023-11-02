eac801d2ae0e738565973c931099e194be032db0
n1hadd <148866867+n1hadd@users.noreply.github.com>
2023-11-02 12:56:46 +0100

    Add files via upload

--- nal7 - OBRAZEC/zoom.js
+++ nal7 - OBRAZEC/zoom.js
@@ -1,10 +1,10 @@
 let height = screen.height;
 let width = screen.width;
-if(height < 800){
-    document.body.style.zoom = "75%"
-}
-else if(width <= 1920 && height <= 1080){
+if(width <= 1920 && height <= 1080){
     document.body.style.zoom = "90%";
+}
+else if(height <= 800){
+    document.body.style.zoom = "75%"
 }
 
 
