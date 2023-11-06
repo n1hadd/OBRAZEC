2416ca0d000f67c31eeac82f4f6973d18404b8b1
n1hadd <148866867+n1hadd@users.noreply.github.com>
2023-11-06 19:21:16 +0100

    Add files via upload

--- nal7 - OBRAZEC/zoom.js
+++ nal7 - OBRAZEC/zoom.js
@@ -1,14 +1,24 @@
-let height = screen.height;
-let width = screen.width;
-if(width <= 1920 && height <= 1080){
-    document.body.style.zoom = "90%";
+function adjustZoomLevel() {
+    let height = screen.height;
+	let width = screen.width;
+
+	if (width === 1920 && height === 1080) {
+		document.body.style.zoom = "100%";
+	} else if (width === 1280 && height === 720) {
+		document.body.style.zoom = "75%";
+	} else if (width === 2560 && height === 1440) {
+		document.body.style.zoom = "100%";
+	} else if (width === 3840 && height === 2160) {
+		document.body.style.zoom = "120%";
+	} else if (width === 800 && height === 600) {
+		document.body.style.zoom = "60%";
+	} else if (width === 1024 && height === 768) {
+		document.body.style.zoom = "75%";
+	} else {
+		// Dodajte morebitne druge ločljivosti, ki jih želite podpreti
+		document.body.style.zoom = "100%"; // Default vrednost za ostale ločljivosti
+	}
+
 }
-else if(height <= 800){
-    document.body.style.zoom = "75%"
-}
 
-
-   
-
-
-
+adjustZoomLevel();
